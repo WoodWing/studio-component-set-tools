03745c3c338d75aa961d906e02428e36706258e3
Sander van Noort <sno@woodwing.com>
2018-02-19 15:54:24 +0100

    validator CSH-3279: add jest test runner. Start test for a minimal component package sample. Update schema with various for properties for the components.

--- lib/components-schema.ts
+++ lib/components-schema.ts
@@ -3,16 +3,23 @@
 */
 
 export const componentsDefinitionSchema = {
-    type: "object",
+    type: 'object',
     properties: {
+        name: { type: 'string', description: 'Name of the components package' },
+        description: { type: 'string', description: 'Description of components package' },
+        version: { type: 'string', description: 'Version of matching components model' },
+
         // Describes components
         components: {
-            type: "array",
+            type: 'array',
             items: {
-                type: "object",
+                type: 'object',
                 properties: {
-                    name: { type: "string" },
-                    label: { type: "string" },
+                    name: { type: 'string', description: 'Component identifier. Must be unique' },
+                    label: { type: 'string', description: 'Component label shown in Digital Editor' },
+                    icon: { type: 'string', description: 'Icon shown for component in Digital Editor' },
+                    properties: { type: 'array', items: { type: 'string' }, description: 'names of properties this component can use.' },
+                    countStatistics: { type: 'boolean', description: 'Count characters, words and paragraphs of this component' },
                 },
                 additionalProperties: false
             }
@@ -20,17 +27,17 @@ export const componentsDefinitionSchema = {
 
         // Properties available in component
         componentProperties: {
-            type: "array"
+            type: 'array'
         },
 
         // Groups component chooser
         groups: {
-            type: "array"
+            type: 'array'
         },
 
         // Conversion rules for transforming one component into another
         conversionRules: {
-            type: "object"
+            type: 'object'
         }
     },
     additionalProperties: false,
