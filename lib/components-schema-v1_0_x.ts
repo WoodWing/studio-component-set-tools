1674a20ef0db71887a456b389acb9a37708b1cf5
Joost Huizinga <jhu@woodwing.com>
2018-07-12 11:37:40 +0200

    CSH-3277 - Remove unneeded schema properties

--- lib/components-schema-v1_0_x.ts
+++ lib/components-schema-v1_0_x.ts
@@ -24,8 +24,6 @@ const labelProperty = (description: string ) => {
                     values: {
                         type: 'object',
                         description: 'String replacement variables',
-                        properties: {},
-                        additionalProperties: true,
                     },
                 },
                 additionalProperties: false,
