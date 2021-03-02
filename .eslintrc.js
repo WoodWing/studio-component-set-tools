9f39cee1e1298986dce24b0e43a1989365a4a429
Ruud Jungbacker <rju@woodwing.com>
2021-03-02 16:01:53 +0100

    Replaced tslint with eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        'object-shorthand': ['error', 'never'],
+    },
+};
