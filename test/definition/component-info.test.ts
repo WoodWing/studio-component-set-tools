d8a3a693efd01c4df3a118236e2887bd4046e4b6
Ruud Jungbacker <rju@woodwing.com>
2021-03-25 20:01:10 +0100

    Add a test for the restrictChildren field in the component info.

--- test/definition/component-info.test.ts
+++ test/definition/component-info.test.ts
@@ -4,16 +4,8 @@ import { processInfo } from '../../lib/definition/component-info';
 import { processTemplates } from '../../lib/definition/process-templates';
 import { ComponentsDefinition } from '../../lib/models';
 
-describe('Component Info', () => {
-    const componentPath = path.resolve('./test/resources/minimal-sample');
-    let definition: ComponentsDefinition;
-    beforeEach(async () => {
-        definition = JSON.parse(
-            (await fs.promises.readFile(path.join(componentPath, 'components-definition.json'))).toString(),
-        );
-    });
-
-    async function processTemplatesFromZip() {
+describe('processInfo', () => {
+    async function processTemplatesFromZip(componentPath: string, definition: ComponentsDefinition) {
         await processTemplates(async (relativePath: string) => {
             const file = path.join(componentPath, relativePath);
             try {
@@ -24,15 +16,23 @@ describe('Component Info', () => {
         }, definition);
     }
 
-    async function processEmptyTemplates() {
-        await processTemplates(async (_relativePath: string) => {
-            return '<div></div>';
-        }, definition);
-    }
+    describe('minimal component set', () => {
+        const componentPath = path.resolve('./test/resources/minimal-sample');
+        let definition: ComponentsDefinition;
+        beforeEach(async () => {
+            definition = JSON.parse(
+                (await fs.promises.readFile(path.join(componentPath, 'components-definition.json'))).toString(),
+            );
+        });
 
-    describe('processInfo', () => {
+        async function processEmptyTemplates() {
+            await processTemplates(async (_relativePath: string) => {
+                return '<div></div>';
+            }, definition);
+        }
+
         it('should return the component set information data', async () => {
-            await processTemplatesFromZip();
+            await processTemplatesFromZip(componentPath, definition);
 
             expect(processInfo(definition)).toEqual({
                 components: {
@@ -66,6 +66,83 @@ describe('Component Info', () => {
                     },
                     intro: {
                         fields: [],
+                    },
+                },
+            });
+        });
+    });
+
+    describe('restrict children sample component set', () => {
+        const componentPath = path.resolve('./test/resources/restrict-children-sample');
+        let definition: ComponentsDefinition;
+        beforeEach(async () => {
+            definition = JSON.parse(
+                (await fs.promises.readFile(path.join(componentPath, 'components-definition.json'))).toString(),
+            );
+        });
+
+        it('should return the component set information data', async () => {
+            await processTemplatesFromZip(componentPath, definition);
+
+            expect(processInfo(definition)).toEqual({
+                components: {
+                    image: {
+                        fields: [
+                            {
+                                contentKey: 'image',
+                                type: 'image',
+                            },
+                            {
+                                contentKey: 'hyperlink',
+                                type: 'link',
+                            },
+                            {
+                                contentKey: 'caption',
+                                type: 'editable',
+                            },
+                        ],
+                    },
+                    slideshow: {
+                        fields: [
+                            {
+                                contentKey: 'slideshow',
+                                type: 'slideshow',
+                            },
+                        ],
+                    },
+                    body: {
+                        fields: [
+                            {
+                                contentKey: 'text',
+                                type: 'editable',
+                            },
+                        ],
+                    },
+                    intro: {
+                        fields: [
+                            {
+                                contentKey: 'text',
+                                type: 'editable',
+                            },
+                        ],
+                    },
+                    container: {
+                        fields: [
+                            {
+                                contentKey: 'main',
+                                restrictChildren: ['body', 'intro'],
+                                type: 'container',
+                            },
+                        ],
+                    },
+                    container2: {
+                        fields: [
+                            {
+                                contentKey: 'main',
+                                restrictChildren: ['slideshow', 'image', 'body', 'intro', 'container'],
+                                type: 'container',
+                            },
+                        ],
                     },
                 },
             });
