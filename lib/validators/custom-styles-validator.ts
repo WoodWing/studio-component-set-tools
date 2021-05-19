e26ced8398f1209f90f0f6fa16a19ad030f9ac22
Ruud Jungbacker <rju@woodwing.com>
2021-05-19 21:43:09 +0200

    custom-styles ECS-409: Added checks for the default of the custom style.

--- lib/validators/custom-styles-validator.ts
+++ lib/validators/custom-styles-validator.ts
@@ -1,5 +1,6 @@
 import { ComponentSet } from '../models';
 import { Validator } from './validator';
+import * as path from 'path';
 
 export class CustomStylesValidator extends Validator {
     constructor(error: (errorMessage: string) => false, definition: ComponentSet, private filePaths: Set<string>) {
@@ -12,6 +13,14 @@ export class CustomStylesValidator extends Validator {
         if (!this.componentSet.customStyles) {
             return;
         }
+
+        this.componentSet.customStyles.forEach((customStyle) => {
+            if (customStyle.default) {
+                if (!this.filePaths.has(path.normalize(customStyle.default))) {
+                    this.error(`The default file for custom style "${customStyle.label}" does not exist`);
+                }
+            }
+        });
     }
 
     private checkCustomStylesFolder() {
--- test/validators/custom-styles-validator.test.ts
+++ test/validators/custom-styles-validator.test.ts
@@ -9,17 +9,70 @@ describe('CustomStylesValidator', () => {
     beforeEach(() => {
         // valid definition
         definition = {
-            components: {},
+            customStyles: [],
         };
         error = jasmine.createSpy('error');
     });
 
     describe('validate', () => {
-        it('should error when a styles/customStyles directory is found', async () => {
+        it('should do nothing when no custom styles are given', async () => {
+            delete definition.customStyles;
+            validator = new CustomStylesValidator(error, definition, new Set());
+            await validator.validate();
+            expect(error).not.toHaveBeenCalled();
+        });
+
+        it('should do nothing when no custom styles are given', async () => {
+            validator = new CustomStylesValidator(error, definition, new Set());
+            await validator.validate();
+            expect(error).not.toHaveBeenCalled();
+        });
+
+        it(`should throw no errors when the style doesn't have a default defined`, async () => {
+            definition.customStyles.push({
+                key: 'test-style',
+                label: 'Test Style',
+                type: 'JSON',
+            });
+            validator = new CustomStylesValidator(error, definition, new Set());
+            await validator.validate();
+            expect(error).not.toHaveBeenCalled();
+        });
+
+        it(`should throw now errors when the default file is available`, async () => {
+            definition.customStyles.push({
+                key: 'test-style',
+                label: 'Test Style',
+                type: 'JSON',
+                default: './styles/default-custom-styles/existing-file.json',
+            });
             validator = new CustomStylesValidator(
                 error,
                 definition,
-                new Set<string>([path.normalize('styles/customStyles/apple-news.json')]),
+                new Set([path.normalize('styles/default-custom-styles/existing-file.json')]),
+            );
+            await validator.validate();
+            expect(error).not.toHaveBeenCalled();
+        });
+
+        it(`should error when the default file isn't available`, async () => {
+            definition.customStyles.push({
+                key: 'test-style',
+                label: 'Test Style',
+                type: 'JSON',
+                default: 'non-existing-file.json',
+            });
+            validator = new CustomStylesValidator(error, definition, new Set());
+            await validator.validate();
+            expect(error).toHaveBeenCalledWith('The default file for custom style "Test Style" does not exist');
+        });
+
+        it('should error when a styles/customStyles directory is found', async () => {
+            delete definition.customStyles;
+            validator = new CustomStylesValidator(
+                error,
+                definition,
+                new Set([path.normalize('styles/customStyles/apple-news.json')]),
             );
             await validator.validate();
             expect(error).toHaveBeenCalledWith(
