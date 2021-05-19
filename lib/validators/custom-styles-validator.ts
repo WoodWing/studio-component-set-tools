cec7d43984aa5a3be4d8cf07bf93f894f5a8eaba
Ruud Jungbacker <rju@woodwing.com>
2021-05-19 21:51:05 +0200

    custom-styles ECS-409: Minor rework in the custom styles validator.

--- lib/validators/custom-styles-validator.ts
+++ lib/validators/custom-styles-validator.ts
@@ -15,16 +15,18 @@ export class CustomStylesValidator extends Validator {
         }
 
         this.componentSet.customStyles.forEach((customStyle) => {
-            if (customStyle.default) {
-                if (!this.filePaths.has(path.normalize(customStyle.default))) {
-                    this.error(`The default file for custom style "${customStyle.label}" does not exist`);
-                }
+            if (!customStyle.default) {
+                return;
+            }
+
+            if (!this.filePaths.has(path.normalize(customStyle.default))) {
+                this.error(`The default file for custom style "${customStyle.label}" does not exist`);
             }
         });
     }
 
     private checkCustomStylesFolder() {
-        if (!Array.from(this.filePaths).every((file) => !file.toLowerCase().startsWith('styles/customstyles/'))) {
+        if (Array.from(this.filePaths).some((file) => file.toLowerCase().startsWith('styles/customstyles/'))) {
             this.error(`The "styles" directory can't contain a directory called "customStyles".`);
         }
     }
